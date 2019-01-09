f3f42db9ce445a9f025c51a1f2a14d323810d651
Hagen Hübel <hhuebel@itinance.com>
2019-01-09 14:15:21 +0100

    BigNumber was removed from web3, need to import it from web3.utils

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -8,10 +8,12 @@ const OwnableMultiple = artifacts.require('./OwnableMultiple.sol');
 
 const ARTWORK_ID_DEFAULT = 'artwork_0';
 
+const BigNumber = web3.utils.BN;
+
 module.exports = function(deployer, network, accounts) {
     const openingTime = web3.eth.getBlock('latest').timestamp + 2; // two secs in the future
     const closingTime = openingTime + 86400 * 30; // 30 days
-    const rate = new web3.BigNumber(1000);
+    const rate = new BigNumber(1000);
     const wallet = accounts[1];
 
 
